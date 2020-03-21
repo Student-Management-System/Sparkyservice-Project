f092f6ceb6ae394f9a83d6744e2213bd8da53f28
Maze7 <sparkm@uni-hildesheim.de>
2020-03-21 16:27:50 +0100

    JUnit timeout in IntegrationTests annotation

--- sparkyservice-api/src/test/java/net/ssehub/sparkyservice/api/testconf/IntegrationTest.java
+++ sparkyservice-api/src/test/java/net/ssehub/sparkyservice/api/testconf/IntegrationTest.java
@@ -4,7 +4,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 @Test
 @Retention(value = RetentionPolicy.RUNTIME)
+@Timeout(300)
 public @interface IntegrationTest {}
