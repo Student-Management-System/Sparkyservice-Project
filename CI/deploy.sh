f3199f5c1d1567a940375304dcc19f6cbd3eeb96
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2020-03-31 07:00:20 +0200

    Fixes deplay script
    wildcat in file name seems not to work in scp command.

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-jarName=sparkyservice-api-*-spring-boot.jar
-scp -i ~/.ssh/id_rsa_student_mgmt_backend ../sparkyservice-api/target/"${jarName}" elscha@147.172.178.30:~/Sparky
+jarFile=$(find ../sparkyservice-api/target/ -name "*.jar")
+scp -i ~/.ssh/id_rsa_student_mgmt_backend "${jarFile}" elscha@147.172.178.30:~/Sparky
 ssh -i ~/.ssh/id_rsa_student_mgmt_backend elscha@147.172.178.30 'systemctl --user restart sparky-spring-boot'
