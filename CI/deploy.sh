9f39ddde900994d38d55789b948e730e6213c400
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2020-04-02 07:38:55 +0200

    Fixes deploay script
    After integration of the two separate projects into one project, project structure has changed. This is reflected in deploy script.

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-jarFile=$(find ../sparkyservice-api/target/ -name "*-spring-boot.jar")
+jarFile=$(find ../target/ -name "*-spring-boot.jar")
 scp -i ~/.ssh/id_rsa_student_mgmt_backend "${jarFile}" elscha@147.172.178.30:~/Sparky
 ssh -i ~/.ssh/id_rsa_student_mgmt_backend elscha@147.172.178.30 'systemctl --user restart sparky-spring-boot'
