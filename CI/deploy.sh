75e2f1a1e2172a463774d157a2db81ddcd8c635a
Marcel <sparkm@uni-hildesheim.de>
2020-04-08 10:52:53 +0200

    Copy without version information

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -3,7 +3,7 @@ jarFile=$(find ../target/ -name "*-spring-boot.jar")
 
 # Copy Unit & JAR
 scp -i ~/.ssh/id_rsa_student_mgmt_backend "sparky-spring-boot.service" elscha@147.172.178.30:~/.config/systemd/user
-scp -i ~/.ssh/id_rsa_student_mgmt_backend "${jarFile}" elscha@147.172.178.30:~/Sparky
+scp -i ~/.ssh/id_rsa_student_mgmt_backend "${jarFile}" elscha@147.172.178.30:~/Sparky/sparkyservice-spring-boot.jar
 
 # Update service
 ssh -i ~/.ssh/id_rsa_student_mgmt_backend elscha@147.172.178.30 'systemctl --user daemon-reload'
