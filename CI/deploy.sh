c6eb2286b9db4cb73a910fb0b19b501a32d9e48f
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2020-04-08 08:47:55 +0200

    Adds support to update systemd unit

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 jarFile=$(find ../target/ -name "*-spring-boot.jar")
+
+# Copy Unit & JAR
+scp -i ~/.ssh/id_rsa_student_mgmt_backend "sparky-spring-boot.service" elscha@147.172.178.30:~/.config/systemd/user
 scp -i ~/.ssh/id_rsa_student_mgmt_backend "${jarFile}" elscha@147.172.178.30:~/Sparky
+
+# Update service
+ssh -i ~/.ssh/id_rsa_student_mgmt_backend elscha@147.172.178.30 'systemctl --user daemon-reload'
 ssh -i ~/.ssh/id_rsa_student_mgmt_backend elscha@147.172.178.30 'systemctl --user restart sparky-spring-boot'
