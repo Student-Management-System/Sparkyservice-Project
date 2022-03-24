d61332c1633b3b48aa1a95ec48dc5552cf884a63
mspark <sparkm@uni-hildesheim.de>
2022-03-24 09:42:52 +0100

    refactor: better log output on LDAP error
    Like the CommunicationException an UncategorizedLdapException can be
    thrown during an authentication attempt through a missconfigured ldap
    cannot testes in beforehand

--- src/main/java/net/ssehub/sparkyservice/api/GlobalResponseExceptionHandler.java
+++ src/main/java/net/ssehub/sparkyservice/api/GlobalResponseExceptionHandler.java
@@ -1,9 +1,12 @@
 package net.ssehub.sparkyservice.api;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ldap.CommunicationException;
+import org.springframework.ldap.UncategorizedLdapException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +21,8 @@ import net.ssehub.sparkyservice.api.util.ErrorDtoBuilder;
 
 @RestControllerAdvice
 public class GlobalResponseExceptionHandler extends ResponseEntityExceptionHandler {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalResponseExceptionHandler.class);
 
     @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
     protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
@@ -48,10 +53,14 @@ public class GlobalResponseExceptionHandler extends ResponseEntityExceptionHandl
         return handleExceptionInternal(ex, responseDto, new HttpHeaders(), status, request);
     }
     
-    @ExceptionHandler(value = { org.springframework.ldap.CommunicationException.class })
-    protected ResponseEntity<Object> handleLdapConnectionError(org.springframework.ldap.CommunicationException ex, WebRequest request) {
-        LoggerFactory.getLogger(GlobalResponseExceptionHandler.class)
-            .warn("Misconfigured LDAP connection - could not complete authentication request");
-        return handleExceptionInternal(ex, "Could not complete request", new HttpHeaders(), HttpStatus.FORBIDDEN, request);
+    @ExceptionHandler(value = { CommunicationException.class, UncategorizedLdapException.class })
+    protected ResponseEntity<Object> handleLdapConnectionError(org.springframework.ldap.CommunicationException ex, 
+            WebRequest request) {
+        var status = HttpStatus.UNAUTHORIZED;
+        var responseDto = new ErrorDtoBuilder("Could not complete authentication request.", status).build();
+        LOG.warn("Could not complete authentication request due do a LDAP connection error"
+                + " (most likely due to incorrect configuration). "
+                + "This may effect affect other realms below the authentication chain.");
+        return handleExceptionInternal(ex, responseDto, new HttpHeaders(), status, request);
     }
 }
